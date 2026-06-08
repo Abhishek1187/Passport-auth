37e349140e2ec4e89d48c2b8e567fc6ec9104af0
Abhishek <abhishekkumar199671@gmail.com>
2026-06-09 00:29:04 +0530

    api working

--- src/strategies/local-strategy.js
+++ src/strategies/local-strategy.js
@@ -1,5 +1,25 @@
 import passport from 'passport';
 import {strategy} from 'passport-local';
+import {mockUsers} from './mockUsers.js';
 
 
-passport.use()
+passport.use(
+   new strategy ((username , password , done) =>{
+    console.log(`Username: ${username}`)
+    console.log(`Password: ${password}`)
+    try {
+      const findone = mockUsers.find((user) => user.username === username);
+    if(!findone) throw new Error ("email invalid")
+      if(findone.passoword !== password)
+      throw new error("invalid credentials ");
+      done(null , findone);
+    } catch (err) {
+      done(err , null);
+      
+    }
+    
+  
+
+
+   }) 
+)
